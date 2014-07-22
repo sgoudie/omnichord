ed4e5e4b1296b06676cda99c87505abcbc6272e4
sgoudie <hello@samuelgoudie.co.uk>
2014-07-22 11:11:13 +0100

    Initial Commit
    Omnichord instrument in G Major. Sounds generated with Web Audio.

---
+++ js/main.js
@@ -0,0 +1,103 @@
+$(document).ready(function(){
+
+	var context = new webkitAudioContext();
+
+	var playNote = function(target){ //target later mapped to this
+	var osc1 = context.createOscillator(),
+		osc2 = context.createOscillator(),
+		osc3 = context.createOscillator(),
+
+		gain1 = context.createGain();
+		gain2 = context.createGain();
+		gain3 = context.createGain();
+
+		lfo = context.createOscillator(),
+		lfoAmp = context.createGain();
+		gainNode = context.createGain();
+		
+		baseFrequency = notes[target.data("note")];
+
+		osc1.type = osc1Wave;
+		osc2.type = osc2Wave;
+		osc3.type = osc3Wave;
+		lfo.type = lfoWave;
+	
+		if(osc1OctUD == 1){
+			osc1.frequency.value = (baseFrequency*osc1Oct)+osc1Detune;
+		}else{
+			osc1.frequency.value = (baseFrequency/osc1Oct)+osc1Detune;
+		}
+		
+		if(osc2OctUD == 1){
+			osc2.frequency.value = (baseFrequency*osc2Oct)+osc2Detune;
+		}else{
+			osc2.frequency.value = (baseFrequency/osc2Oct)+osc2Detune;
+		}
+
+		if(osc3OctUD == 1){
+			osc3.frequency.value = (baseFrequency*osc3Oct)+osc3Detune;
+		}else{
+			osc3.frequency.value = (baseFrequency/osc3Oct)+osc3Detune;
+		}
+		
+		
+		lfo.frequency.value = lfoSpeed;
+
+		gain1.gain.value = osc1Gain;
+		gain2.gain.value = osc2Gain;
+		gain3.gain.value = osc3Gain;
+
+		lfoAmp.gain.value = lfoStrength; //strength of lfo
+
+		lfo.connect(lfoAmp);
+		lfoAmp.connect(osc1.frequency);
+		lfoAmp.connect(osc2.frequency);
+		lfoAmp.connect(osc3.frequency);
+
+		//route osc individual gains, to master gain node, to speakers.
+		osc1.connect(gain1);
+		osc2.connect(gain2);
+		osc3.connect(gain3);
+		gain1.connect(gainNode);
+		gain2.connect(gainNode);
+		gain3.connect(gainNode);
+		gainNode.connect(context.destination);
+
+		osc1.noteOn(0);
+		osc2.noteOn(0);
+		osc3.noteOn(0);
+		lfo.noteOn(0);
+		currentOscillator1 = osc1;
+		currentOscillator2 = osc2;
+		currentOscillator3 = osc3;
+		currentLfo = lfo;
+
+		now = context.currentTime;
+		gainNode.gain.cancelScheduledValues(now);
+		gainNode.gain.setValueAtTime(0, now);
+		gainNode.gain.linearRampToValueAtTime(0.6, now + attackTime);
+
+		currentGain = gainNode;
+		};
+
+	var stopNote = function (){
+		now = context.currentTime;
+		currentGain.gain.linearRampToValueAtTime(0, now + attackTime + releaseTime);
+		currentOscillator1.noteOff(now + attackTime + releaseTime +1); //stops osc after 0 seconds
+		currentOscillator2.noteOff(now + attackTime + releaseTime +1); //stops osc after 0 seconds
+		currentOscillator3.noteOff(now + attackTime + releaseTime +1); //stops osc after 0 seconds
+		currentLfo.noteOff(now + attackTime + releaseTime +1); //stops lfo after 0 seconds
+		//currentOscillator1.disconnect(0);
+		//currentOscillator2.disconnect(0);
+		//currentOscillator3.disconnect(0);
+		//currentLfo.disconnect(0);
+	}
+
+	$(".trigger").mouseenter(function(){
+		playNote($(this));//grabs note data from trigger
+	});
+
+	$(".trigger").mouseleave(function(){
+	    stopNote();
+	});
+});
---
+++ js/voice.js
@@ -0,0 +1,35 @@
+//Voice Controls
+
+//Amp Envelope
+releaseTime = 1;
+attackTime = 1;
+
+//LFO
+lfoSpeed = 4;
+lfoStrength = 4;
+lfoWave = "sawtooth";
+
+//oscillator 1
+osc1Wave = "sine";
+osc1Gain = 0.2;
+osc1OctUD = 0; //0 - octave goes down, 1 - octave goes up
+osc1Oct = 1; //(1 for base, 2 for 1 octave, 4 for 2 octaves etc)
+osc1Detune = 0;
+
+//oscillator 2
+osc2Wave = "triangle";
+osc2Gain = 0.2;
+osc2OctUD = 1; //0 - octave goes down, 1 - octave goes up
+osc2Oct = 2; //(1 for base, 2 for 1 octave, 4 for 2 octaves etc)
+osc2Detune = 0;
+
+//oscillator 3
+osc3Wave = "sine";
+osc3Gain = 0.2;
+osc3OctUD = 1; //0 - octave goes down, 1 - octave goes up
+osc3Oct = 4; //(1 for base, 2 for 1 octave, 4 for 2 octaves etc)
+osc3Detune = 0;
+
+		
+		
+		
